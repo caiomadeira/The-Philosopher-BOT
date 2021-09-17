34885edf8b01e8e364b6cb04f8406580c6a92d69
Rodrigo Carmo <rodrigoblock@outlook.com>
2021-09-17 01:33:08 -0300

    Updates

--- Logs/Twitter/log_engine.py
+++ Logs/Twitter/log_engine.py
@@ -23,7 +23,7 @@ class LogEngine:
 
             formatter_bot = logging.Formatter('%(asctime)s - %(levelname)s - HASHTAG --> %(message)s')
 
-            file_handler_info = TimedRotatingFileHandler(rf'{PATH_LOG_HASHTAG_INFO}\HASHTAG.log', when='d', interval=1)
+            file_handler_info = TimedRotatingFileHandler(rf'{PATH_LOG_HASHTAG_INFO}\HASHTAG.log', when='midnight', interval=1)
             file_handler_info.setLevel(logging.DEBUG)
             file_handler_info.setFormatter(formatter_bot)
 
